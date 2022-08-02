6d51c92b03ed0cedc2db1237ae0aee58daf20519
Ibeh Esther <estheribeh5@gmail.com>
2022-08-03 00:44:56 +0100

    refactor: set up default port for Flask application

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
 
 # Or specify port manually:
 
-if __name__ == '__main__':
-    app.debug = True
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == '__main__':
+#     app.debug = True
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(host='0.0.0.0', port=port)
