6ea31bc1775bb06845c16c7ddc44e1ea2f09753b
Ibeh Esther <estheribeh5@gmail.com>
2022-08-03 01:29:42 +0100

    refactor: Update from database uri
    changed from dialect name from postgres to postgresql as SQLAlchemy no longer supports the former

--- config.py
+++ config.py
@@ -13,5 +13,5 @@ DEBUG = True
 # Database URI for development
 # SQLALCHEMY_DATABASE_URI = 'postgresql://fsnd:fsnd@localhost:5432/fyyur-app'
 # Database uri for production
-SQLALCHEMY_DATABASE_URI= 'postgres://tmjtdyscympkim:ffef5e10b77aeeb6d2cd374f11640bc1d6f458171db291d4dd71bc491723b7e9@ec2-107-22-122-106.compute-1.amazonaws.com:5432/d1q335bmo2sl5h'
+SQLALCHEMY_DATABASE_URI= 'postgresql://tmjtdyscympkim:ffef5e10b77aeeb6d2cd374f11640bc1d6f458171db291d4dd71bc491723b7e9@ec2-107-22-122-106.compute-1.amazonaws.com:5432/d1q335bmo2sl5h'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
